adec68ee3aa746a17db9b9bcc17e54cd9d834a9a
Yegor Knyazev <egorknyazev10@gmail.com>
2024-05-21 16:18:42 +0400

    add letter-spacing normal

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -6,7 +6,7 @@ export default defineNuxtPlugin((nuxtApp) => {
 	const vuetify = createVuetify({
 		defaults: {
 			VBtn: {
-				style: 'text-transform: none;',
+				style: 'text-transform: none; letter-spacing: normal',
 			},
 			global: {
 				ripple: false,
