ac12eb65a327569fe1d25133afc344768e1cb198
Yegor Knyazev <egorknyazev10@gmail.com>
2024-10-31 19:39:50 +0400

    index page: remove some shit

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,10 @@
 <script lang="ts" setup>
 let auth = useAuth()
-console.log(auth.user.value)
-if (!process.server) {
-  setTimeout(() => { fetch('http://localhost:4000/auth/logout', { method: "POST", credentials: "include" }); console.log('dfgdfg') }, 5000)
-  watch(auth.user, value => console.log(value))
 }
 </script>
 
 <template>
   <v-container>
-    {{ useState('user').value }}
   </v-container>
 </template>
 
