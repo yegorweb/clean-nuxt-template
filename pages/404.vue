71aefda2e591c87faeb60bc7f28fd2614be372d6
Yegor Knyazev <egorknyazev10@gmail.com>
2024-10-31 19:05:11 +0400

    add 404 page

---
+++ pages/404.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+let router = useRouter()
+</script>
+
+<template>
+  <v-container>
+    <BackButton />
+
+    <div class="mt-4 d-flex flex-column align-center">
+      <div 
+        style="font-size: 66px; line-height: 1;" 
+        class="font-weight-bold"
+      >
+        404
+      </div>
+
+      <div>Не найдено</div>
+
+      <v-btn 
+        @click="router.back()"
+        size="small"
+        variant="tonal"
+        class="pr-5 pl-5 mt-4 font-weight-semibold"
+      >
+        Назад
+      </v-btn>
+    </div>
+  </v-container>
+</template>
