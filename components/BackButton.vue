9c114e8c72b7d3aa4eaf957680d7fdaa9f21bf9f
Yegor Knyazev <egorknyazev10@gmail.com>
2024-10-31 19:08:30 +0400

    BackButton: router --> $router

--- components/BackButton.vue
+++ components/BackButton.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { useRouter } from "vue-router";
 
-let router = useRouter()
 </script>
 
 <template>
   <div 
-    @click="router.back()" 
+    @click="$router.back()" 
     class="d-flex pt-1 pr-1 pb-1 cursor-pointer flex-row flex-nowrap align-center justify-start"
   >
     <v-icon>mdi-arrow-left</v-icon>
