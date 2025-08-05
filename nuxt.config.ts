93e395ac63c792e37dfe07f967240adca6107fa2
Yegor Knyazev <egorknyazev10@gmail.com>
2025-08-05 13:41:37 +0400

    install @nuxtjs/sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/robots'],
+  modules: ['@nuxtjs/robots', '@nuxtjs/sitemap'],
 
   css: ['~/assets/styles/main.scss'],
 
