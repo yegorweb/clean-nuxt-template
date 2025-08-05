2adc65980dffbf9b4bc3c0da01ed870499fa5fe2
Yegor Knyazev <egorknyazev10@gmail.com>
2025-08-05 13:06:35 +0400

    add NUXT_DEV_PORT variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    port: Number(process.env.NUXT_DEV_PORT || 3000)
+  },
 })
