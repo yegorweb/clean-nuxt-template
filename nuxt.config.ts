87b47e70f783eab320e528fbe91dbb210e887a95
Yegor Knyazev <egorknyazev10@gmail.com>
2025-08-05 13:28:18 +0400

    add ~assets/styles/main.scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  css: ['~/assets/styles/main.scss'],
+
   devServer: {
     port: Number(process.env.NUXT_DEV_PORT || 3000)
   },
