5e9b06a2fbb0966a7052c5ff6d71b44a5d88d3b3
Yegor Knyazev <egorknyazev10@gmail.com>
2025-09-13 21:17:12 +0400

    add error message component

---
+++ app/components/ErrorMessage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+let props = defineProps<{ message: string | undefined }>()
+let errorMessage = props.message ? `: ${props.message}` : ''
+</script>
+
+<template>
+  <div class="error">Ошибка{{ errorMessage }}</div>
+</template>
+
+<style lang="postcss" scoped>
+.error {
+  font-size: 16px;
+  color: red;
+}
+</style>
