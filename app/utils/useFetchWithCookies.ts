1bc79bf190c08157b0bbbd80d822de915f818f3a
Yegor Knyazev <egorknyazev10@gmail.com>
2025-09-14 09:04:11 +0400

    add fetch with cookies

---
+++ app/utils/useFetchWithCookies.ts
@@ -0,0 +1,24 @@
+import { appendResponseHeader } from 'h3'
+import type { H3Event } from 'h3'
+import type { NitroFetchOptions, NitroFetchRequest } from 'nitropack'
+
+export const useFetchWithCookies = async <
+  DefaultT = unknown,
+  DefaultR extends NitroFetchRequest = NitroFetchRequest,
+  T = DefaultT,
+  R extends NitroFetchRequest = DefaultR,
+  O extends NitroFetchOptions<R> = NitroFetchOptions<R>
+>(
+  event: H3Event,
+  url: R,
+  options?: O
+) => {
+  const headers = useRequestHeaders(['cookie'])
+  if (options && !options.credentials) options.credentials = 'include'
+  const res = await $fetch.raw<T>(url, options ? Object.assign(options, { headers: {...options.headers, ...headers} }) : { headers, credentials: 'include' })
+  const cookies = res.headers.getSetCookie()
+  for (const cookie of cookies) {
+    appendResponseHeader(event, 'set-cookie', cookie)
+  }
+  return res._data
+}
